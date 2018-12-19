78a05019e2dec55ab72fce5a09dfe0dc51748639
Rosana Souza Damasceno <rsouzada@everis.com>
2018-12-19 12:18:11 -0200

    novo componente botao de salvar

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './shared/title/title.component';
+import { SaveButtonComponent } from './shared/title/save-button/save-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    SaveButtonComponent
   ],
   imports: [
     BrowserModule
