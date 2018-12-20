73aabc41d7ae5c0e24b0b3d3de1f4f45e7c569af
Guilherme Gomes Silva <ggomessi@everis.com>
2018-12-20 10:14:18 -0200

    Estrutura de pastas

---
+++ code/src/app/Home/Projetos/atribuicao-equipe/atribuicao-equipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atribuicao-equipe',
+  templateUrl: './atribuicao-equipe.component.html',
+  styleUrls: ['./atribuicao-equipe.component.css']
+})
+export class AtribuicaoEquipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -2,15 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './shared/title/title.component';
-import { PessoasComponent } from './Pessoas/pessoas/pessoas.component';
+import { PessoasComponent } from './Home/Pessoas/pessoas/pessoas.component';
 import { SaveButtonComponent } from './shared/save-button/save-button.component';
+import { LoginComponent } from './Home/Login/login/login.component';
+import { PrimeiroAcessoComponent } from './Home/Login/primeiro-acesso/primeiro-acesso.component';
+import { EsqueceuSenhaComponent } from './Home/Login/esqueceu-senha/esqueceu-senha.component';
+import { DashboardComponent } from './Home/Dashboard/dashboard/dashboard.component';
+import { NovoProjetoComponent } from './Home/Projetos/novo-projeto/novo-projeto.component';
+import { FechamentoProjetoComponent } from './Home/Projetos/fechamento-projeto/fechamento-projeto.component';
+import { AnexosComponent } from './Home/Projetos/anexos/anexos.component';
+import { EsforcoProjetoComponent } from './Home/Projetos/esforco-projeto/esforco-projeto.component';
+import { AtribuicaoEquipeComponent } from './Home/Projetos/atribuicao-equipe/atribuicao-equipe.component';
+import { NovaChangeComponent } from './Home/Projetos/nova-change/nova-change.component';
+import { CadastroColaboradoresComponent } from './Home/Pessoas/cadastro-colaboradores/cadastro-colaboradores.component';
+import { CadastroPessoasComponent } from './Home/Pessoas/cadastro-pessoas/cadastro-pessoas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
     PessoasComponent,
-    SaveButtonComponent
+    SaveButtonComponent,
+    LoginComponent,
+    PrimeiroAcessoComponent,
+    EsqueceuSenhaComponent,
+    DashboardComponent,
+    NovoProjetoComponent,
+    FechamentoProjetoComponent,
+    AnexosComponent,
+    EsforcoProjetoComponent,
+    AtribuicaoEquipeComponent,
+    NovaChangeComponent,
+    CadastroColaboradoresComponent,
+    CadastroPessoasComponent
   ],
   imports: [
     BrowserModule
