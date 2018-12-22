416f59ed1dbdc54f40988c658ee83667dc8424cf
Rosana Souza Damasceno <rsouzada@everis.com>
2018-12-21 22:48:55 -0200

    alteraçoes de ajuste no css

--- code/src/app/shared/modelos/modelos.component.ts
+++ code/src/app/shared/modelos/modelos.component.ts
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 import { Component, OnInit } from '@angular/core';
 import * as  js from 'jquery';
-=======
-import { Component, OnInit} from '@angular/core';
->>>>>>> 35c8f9bd6e9d69e3e22089f1c31d49b0abcc8746
+
 
 @Component({
   selector: 'app-modelos',
