53dea128cc868cb026c9737c319ed29d44344ebd
nicholasdelatorre <nicholas.delatorre@gmail.com>
2018-12-28 13:06:13 -0200

    Validacao formulario cadastro pessoa

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -34,7 +34,7 @@ import { ProjetoService } from './services/projeto.service';
 import { FormsModule, ReactiveFormsModule } from '../../node_modules/@angular/forms';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
 import { UploadComponent } from './shared/upload/upload.component';
-import { InputComponent } from './shared/input/input.component';
+
 
 
 @NgModule({
@@ -58,8 +58,7 @@ import { InputComponent } from './shared/input/input.component';
     ModelosComponent,
     TemplateComponent,
     EmpresaComponent,
-    UploadComponent,
-    InputComponent
+    UploadComponent
   ],
   imports: [
     HttpClientModule,
