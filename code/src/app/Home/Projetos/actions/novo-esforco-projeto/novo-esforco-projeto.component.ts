eccaa996fb35aa7b668c2257894428347522cd1c
Rosana Souza Damasceno <rsouzada@everis.com>
2018-12-28 17:53:25 -0200

    rotas e mapa do site

---
+++ code/src/app/Home/Projetos/actions/novo-esforco-projeto/novo-esforco-projeto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-esforco-projeto',
+  templateUrl: './novo-esforco-projeto.component.html',
+  styleUrls: ['./novo-esforco-projeto.component.css']
+})
+export class NovoEsforcoProjetoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
