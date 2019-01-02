b35bf9a027b91023b23ccfc597eb0d35302ae3ec
Yondo-r <yondo.ricardo@gmail.com>
2019-01-02 14:37:37 -0200

    ajuste de layout de atribuicao de equipe

--- code/src/app/Home/Pessoas/pessoas.component.ts
+++ code/src/app/Home/Pessoas/pessoas.component.ts
@@ -1,4 +1,6 @@
+import { PessoaService } from 'src/app/_services/pessoa.service';
 import { Component, OnInit } from '@angular/core';
+import { PessoaColaboradorViewModel } from 'src/app/_models/pessoacolaborador.viewmodel';
 
 @Component({
   selector: 'app-pessoas',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pessoaService: PessoaService) { }
+
+  pessoa = new PessoaColaboradorViewModel();
 
   ngOnInit() {
+    this.pessoaService.ObterTodos().subscribe(data => {
+      this.pessoa = data['data'];
+      console.log(this.pessoa);
+    }
+    );
   }
 
 }
