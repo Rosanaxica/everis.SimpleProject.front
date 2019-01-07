49d5a2b9843e9c3900c2809ad041dc4160505db0
Nicholas Dela Torre <nicholas.delatorre@gmail.com>
2019-01-07 17:03:34 -0200

    Alterado metodo de salvar nova  pessoa

--- code/src/app/Home/Pessoas/actions/cadastro-pessoas/cadastro-pessoas.component.ts
+++ code/src/app/Home/Pessoas/actions/cadastro-pessoas/cadastro-pessoas.component.ts
@@ -82,8 +82,8 @@ export class CadastroPessoasComponent implements OnInit {
       pessoaColaborador.colaborador = this.colaborador;
       pessoaColaborador.pessoa = this.pessoa;
 
-      this.svc.salvar(pessoaColaborador, PessoaColaboradorViewModel)
-      .toPromise().then(
+      this.svc.postViewModel(pessoaColaborador, 'pessoa/CriarPessoaColaborador')
+        .toPromise().then(
           data => {
             this.msgSucesso = 'Colaborador cadastrado com sucesso!';
           },
@@ -94,7 +94,7 @@ export class CadastroPessoasComponent implements OnInit {
     } else {
       this.pessoa.empresaId = this.empresaId;
       this.svc.salvar(this.pessoa, Pessoa)
-      .toPromise().then(
+        .toPromise().then(
           data => {
             this.msgSucesso = 'Terceiro cadastrado com sucesso!';
           },
