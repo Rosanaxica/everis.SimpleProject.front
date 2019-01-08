93153b508528e1ec288dfd15cf160475c77412e9
vmonteir <vmonteir@SAO-6H8DCD2>
2019-01-08 15:21:41 -0200

    Nova Change

--- code/src/app/Home/Projetos/actions/nova-change/nova-change.component.ts
+++ code/src/app/Home/Projetos/actions/nova-change/nova-change.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Change } from 'src/app/_models/change.model';
+
 
 @Component({
   selector: 'app-nova-change',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nova-change.component.css']
 })
 export class NovaChangeComponent implements OnInit {
+  [x: string]: any;
 
   constructor() { }
 
+  change = new Change();
+  msgSucesso: String;
+  msgErro: String;
+
   ngOnInit() {
+  }
+
+  Salvar(form: NgForm) {
+    this.svc.salvar(this.change, Change).toPromise().then(
+      data => {
+        this.msgSucesso = 'Cadastro realizado com sucesso!';
+      },
+      error => {
+        this.msgErro = 'Erro ao salvar';
+      }
+    );
+    form.reset();
+  }
+
+  Cancelar() {
+    this.router.navigate(['/template/projetos/novo-projeto/changes']);
   }
 
 }
