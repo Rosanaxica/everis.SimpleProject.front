884004b1e300733f2b9a73a79282496f35aeceff
Guilherme Gomes da Silva <guilherme_gs@live.com>
2019-01-09 17:40:16 -0200

    Alteração Projetos

--- code/src/app/Home/Projetos/actions/novo-projeto/novo-projeto.component.ts
+++ code/src/app/Home/Projetos/actions/novo-projeto/novo-projeto.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { DadosPrincipaisComponent } from './actions/dados-principais/dados-principais.component';
+import { TabsetComponent } from 'ngx-bootstrap';
+import { AtribuicaoEquipeComponent } from './actions/atribuicao-equipe/atribuicao-equipe.component';
+import { AnexosComponent } from './actions/anexos/anexos.component';
 
 @Component({
   selector: 'app-novo-projeto',
@@ -8,6 +12,11 @@ import { Router } from '@angular/router';
 })
 export class NovoProjetoComponent implements OnInit {
 
+  @ViewChild(DadosPrincipaisComponent) formDados: DadosPrincipaisComponent;
+  @ViewChild(AtribuicaoEquipeComponent) formAtribuicaoEquipe: AtribuicaoEquipeComponent;
+  @ViewChild(AnexosComponent) formAnexo: AnexosComponent;
+  @ViewChild('alterarTabs') alterarTabs: TabsetComponent;
+  
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -15,5 +24,17 @@ export class NovoProjetoComponent implements OnInit {
 
   cancelar() {
     this.router.navigate(['/template/projetos']);
+  }
+  Adicionar(){
+    this.formDados.Adicionar();
+    this.selectTab(1);
+  }
+  selectTab(tabId: number) {
+    if(tabId == 1){
+      this.formAtribuicaoEquipe.OpenView("teste")
+    }else if(tabId == 2){
+      this.formAnexo.OpenView("teste")
+    }
+    this.alterarTabs.tabs[tabId].active = true;
   }
 }
