3f39825479943f5b96c6431c0c429ee5dd7b9379
Ricardo Grieco <ricardogrieco@hotmail.com>
2019-01-10 10:32:21 -0200

    Listar esforco-projeto

--- code/src/app/Home/Projetos/actions/esforco-projeto/esforco-projeto.component.ts
+++ code/src/app/Home/Projetos/actions/esforco-projeto/esforco-projeto.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Projeto } from 'src/app/_models/projeto.model';
+import { GenericService } from 'src/app/_services/generic.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,12 +10,24 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./esforco-projeto.component.css']
 })
 export class EsforcoProjetoComponent implements OnInit {
+  constructor(private svc: GenericService, private router: Router) { }
 
-
-  constructor() {
-   }
+  projetos: Projeto[] = [];
+  filtroProjeto = new Projeto();
 
   ngOnInit() {
+    this.filtrar();
   }
 
+  filtrar() {
+    this.svc.listar(Projeto, this.filtroProjeto).toPromise().then(
+      s => {
+        if (s.sucesso) {
+          if (s.data != null && s.data !== undefined) {
+            this.projetos = s.data;
+          }
+        }
+      }
+    );
+  }
 }
