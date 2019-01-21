94ebbe4fe9c16ac78bb9c319759fd93e7ae84cb9
Raoni1984 <raonico@gmail.com>
2019-01-21 10:42:13 -0200

    Ajuste status checkboxes

--- code/src/app/Home/Projetos/projetos.component.ts
+++ code/src/app/Home/Projetos/projetos.component.ts
@@ -21,10 +21,13 @@ export class ProjetosComponent implements OnInit {
   pessoas: any;
   status: Status[] = [];
   statusSelecionados = [
-      {id: 1, descricao: 'Em Aprovação', checked: true},
-      {id: 2, descricao: 'Em Execução', checked: true},
-      {id: 3, descricao: 'Cancelado', checked: true},
-      {id: 4, descricao: 'Concluído', checked: true}
+      {id: 1, descricao: 'Em Desenvolvimento', checked: true},
+      {id: 2, descricao: 'Aguardando Aprovação', checked: true},
+      {id: 3, descricao: 'Concluído', checked: true},
+      {id: 4, descricao: 'Entregue', checked: true},
+      {id: 5, descricao: 'Aguardando Abertura da SS', checked: true},
+      {id: 6, descricao: 'Cancelado', checked: true},
+      {id: 7, descricao: 'Proposta', checked: true}
   ];
   filtroProjeto = new Projeto();
   form: FormGroup;
